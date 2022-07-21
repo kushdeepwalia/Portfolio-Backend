d7308762358b3771c8d7dc9d532121b65f533336
kushdeepwalia <skushdeep5@gmail.com>
2022-07-21 05:45:57 +0530

    changes

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 
 const uri = process.env.MONGO_URI !== undefined ? process.env.MONGO_URI : "mongodb://localhost:27017/PersonalPortfolio";
 
+console.log(uri);
 mongoose.connect(uri, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
