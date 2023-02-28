a139c6d3353904416ee8bb5913056f9d8465c403
kushdeepwalia <skushdeep5@gmail.com>
2023-03-01 00:53:21 +0530

    api correction attempt

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/", (req, res, next) => {
-   res.status(200).json({ message: "Api Working" })
-})
 router.use("/auth", require("./auth.route"));
 router.use("/projects", require("./project.route"))
 router.use("/testimonials", require("./testimonial.route"))
+router.use("/", (req, res, next) => {
+   res.status(200).json({ message: "Api Working" })
+})
 
 module.exports = router;
